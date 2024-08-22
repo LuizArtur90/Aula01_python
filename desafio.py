d25ed66923c4512d98ceecd4c2f6f86df2606b7b
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-22 10:59:36 -0300

    atualização do code

--- desafio.py
+++ desafio.py
@@ -1,8 +1,12 @@
+#Entre com o nome do usuário
 user = input("Digite seu nome:")
+#Entre com o salário do usuário
 salario = float(input("Digite seu salário:"))
+#Entre com o bonus do usuário
 bonus_user = float(input("Digite sue bônus:"))
+#Variável que será alterado anualmente
 percent = 1000
-
+#Cálculo do KPI final
 bonus_final = percent + salario*bonus_user
-
-print(f"Olá {user},o seu bônus foi de {bonus_final}.")
+#Mensagem final
+print(f"Olá {user}, o seu bônus foi de {bonus_final}.")
