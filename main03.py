660e7d6140e3658523ed849c0e10d7e1b31e2a45
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-22 10:21:39 -0300

    Exercício 3 finalizado

---
+++ main03.py
@@ -0,0 +1,7 @@
+#Refatore o exercício 02 atribuindo variáveis
+
+valor1 = int(input("Entre com o primeiro valor:"))
+valor2 = int(input("Entre com o segundo valor:"))
+
+soma_total = (valor1 + valor2)
+print (soma_total)
